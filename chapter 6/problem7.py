54d9c29ae430bc42961654a9fd7c81313bce82aa
Aakarshit Pathak <139079683+aakarshitpathak@users.noreply.github.com>
2025-08-02 00:02:28 +0530

    Delete chapter 6 directory

--- chapter 6/problem7.py
+++
@@ -1,9 +0,0 @@
-post = input("Enter the post: ")
-
-
-
-if("aakarshit" in post.lower()):
-    print("This post is talking about aakarshit")
-
-else:
-    print("This post is not talking about aakarshit")
