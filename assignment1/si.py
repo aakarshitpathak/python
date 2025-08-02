027bb1cc978a59dde3a6750fbe38b260cb936a98
Aakarshit Pathak <139079683+aakarshitpathak@users.noreply.github.com>
2025-08-03 00:29:19 +0530

    Delete assignment1/si.py

--- assignment1/si.py
+++
@@ -1,9 +0,0 @@
-# simple interest program
-
-p=float(input("Enter the principle amount"))
-r=float(input("Enter the rate"))
-t=float(input("Enter the time period"))
-
-si=(p*r*t)/100
-
-print("Simple Interest is:",si)
