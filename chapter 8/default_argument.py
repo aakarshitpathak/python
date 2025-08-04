ff63101b73045ba73c2e4a6d8a5c0147895c8e76
Aakarshit Pathak <139079683+aakarshitpathak@users.noreply.github.com>
2025-08-05 00:13:17 +0530

    Delete chapter 8/default_argument.py

--- chapter 8/default_argument.py
+++
@@ -1,7 +0,0 @@
-def goodDay(name, ending="Thank you"): #here ending is set to by default
-    # i.e.if user forget to add ending then by default will execute
-    print(f"Good Day, {name}")
-    print(ending)
-
-goodDay("Aakarshit", "Thanks")
-goodDay("Rohan")
