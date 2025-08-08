d3813cec44a557bf03d8cad402302d758139f410
Aakarshit Pathak <139079683+aakarshitpathak@users.noreply.github.com>
2025-08-09 00:19:49 +0530

    Delete chapter 9/problem4.py

--- chapter 9/problem4.py
+++
@@ -1,9 +0,0 @@
-word = "Donkey"
-
-with open("mfile.txt" , "r") as f:
-    content = f.read()
-
-contentNew = content.replace(word ,"######")
-
-with open("mfile.txt", "w") as f:
-    f.write(contentNew)
