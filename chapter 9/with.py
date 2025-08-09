514bd26fcea6d3cef1b5a086f66294963b950796
Aakarshit Pathak <139079683+aakarshitpathak@users.noreply.github.com>
2025-08-10 00:10:58 +0530

    Delete chapter 9/with.py

--- chapter 9/with.py
+++
@@ -1,9 +0,0 @@
-f = open("file.txt" ,"r")
-print(f.read())
-f.close()
-
-# The same can be written as
-with open("file.txt")as f:
-    print(f.read())
-
-# now you don't have to write explictly to close the file
