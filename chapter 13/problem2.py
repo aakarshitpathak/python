ae6a04d207d4728fb1d109892a2eeabb759e8e1a
Aakarshit Pathak <139079683+aakarshitpathak@users.noreply.github.com>
2025-08-18 00:03:30 +0530

    Delete chapter 13/problem2.py

--- chapter 13/problem2.py
+++
@@ -1,7 +0,0 @@
-name= input("Enter name: ")
-marks = input("Enter marks: ")
-phone = input("Enter phone numbers:")
-
-s= "The name of the student is {}, his marks are {} and phone number is {} ".format(name, marks, phone)
-
-print(s)
