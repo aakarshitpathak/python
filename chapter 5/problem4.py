20c3373e645db338f08120f1a007798f3c1e00b2
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-09-10 00:08:46 +0530

    commit44

---
+++ chapter 5/problem4.py
@@ -0,0 +1,7 @@
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20') # length of s after these operations?
+
+print(len(s)) #python ki woh property hai ki woh 20.0 aur 20 dono ko 
+# same leta hai check karke
