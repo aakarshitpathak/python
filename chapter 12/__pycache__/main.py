00f9d7774ee3ccf05ba611a940bf14fab092e0a2
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-09-23 00:19:22 +0530

    commit85

--- chapter 12/__pycache__/main.py
+++ chapter 12/__pycache__/main.py
@@ -4,4 +4,4 @@ from module import myFunc
 # woh us file ka naam lata hai __name__ me jaha se woh import kiya gaya
 # hai jaise yaha module se import kiya hai toh woh run nahi karega lekin 
 # yahi cheez hum module file se run karenge toh woh run hoga kyuki waha
-# __name__ me main file ka name pada hua hai
+# __name__ me main file ka name pada hua hai 
