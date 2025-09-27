ad129c7e1c20a33ec5e6b2e12cb0b67e23c4d7bb
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-09-28 00:01:49 +0530

    commit105

---
+++ chapter 7/problem1.py
@@ -0,0 +1,7 @@
+n=int(input("Enter any number: "))
+
+for i in range(1,11):
+    print(f"{n} X {i} = {n*i}")
+    # here f is a f string which helps in adding used variables without any error 
+
+          
