c585afccc157b3048b7e433e9a3052ea321d7776
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-09-30 00:06:49 +0530

    commit114

---
+++ chapter 8/default_argument.py
@@ -0,0 +1,7 @@
+def goodDay(name, ending="Thank you"): #here ending is set to by default
+    # i.e.if user forget to add ending then by default will execute
+    print(f"Good Day, {name}")
+    print(ending)
+
+goodDay("Aakarshit", "Thanks")
+goodDay("Rohan")
